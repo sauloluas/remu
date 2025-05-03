55098cda28a5ed9adfc09f41f24bbfd664fa2970
sauloluas <sauloroba@gmail.com>
2025-05-03 01:08:52 -0300

    feats: pc annotation, and general improvements

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
-use std::fmt::Debug;
+use std::fmt::{self, Debug};
 use std::ops::{Index, IndexMut};
 
 use algin::{Vector, Zero};
 
-pub type DWord = u8;
+pub type Byte = u8;
 pub type IWord = u16;
 
 pub trait Word: Clone + Copy + Default {}
 
-impl Word for DWord {}
+impl Word for Byte {}
 impl Word for IWord {}
 
 pub struct Processor {
@@ -26,14 +26,14 @@ impl Processor {
 
 #[derive(Debug)]
 pub struct RegisterBank {
-    acc: Reg8,
-    bacc: Reg8,
-    carr: Reg8,
-    datt: Reg8,
-    idx: Reg8,
-    hi: Reg8,
-    lo: Reg8,
-    ret: Reg8,
+    acc: Byte,
+    bacc: Byte,
+    carr: Byte,
+    idx: Byte,
+    hi: Byte,
+    lo: Byte,
+    ret: Byte,
+    tmp: Byte,
 }
 pub type RegBank = RegisterBank;
 
@@ -43,55 +43,52 @@ impl RegisterBank {
             acc: 0,
             bacc: 0,
             carr: 0,
-            datt: 0,
             idx: 0,
             hi: 0,
             lo: 0,
             ret: 0,
+            tmp: 0,
         }
     }
 }
 
 impl Index<RegAddress> for RegisterBank {
-    type Output = Reg8;
+    type Output = Byte;
 
-    fn index(&self, idx: RegAddress) -> &Reg8 {
+    fn index(&self, idx: RegAddress) -> &Byte {
         match idx {
             Acc => &self.acc,
             Bacc => &self.bacc,
             Carr => &self.carr,
-            Datt => &self.datt,
             Idx => &self.idx,
             Hi => &self.hi,
             Lo => &self.lo,
             Ret => &self.ret,
+            Tmp => &self.tmp
         }
     }
 }
 
 impl IndexMut<RegAddress> for RegisterBank {
-    fn index_mut(&mut self, idx: RegAddress) -> &mut Reg8 {
+    fn index_mut(&mut self, idx: RegAddress) -> &mut Byte {
         match idx {
             Acc => &mut self.acc,
             Bacc => &mut self.bacc,
             Carr => &mut self.carr,
-            Datt => &mut self.datt,
             Idx => &mut self.idx,
             Hi => &mut self.hi,
             Lo => &mut self.lo,
             Ret => &mut self.ret,
+            Tmp => &mut self.tmp
         }
     }
 }
-
-pub type Reg8 = u8;
-pub type Reg16 = u16;
 
 pub struct Memory<T: Word> {
     cells: Vector<T>,
 }
 pub type Mem<T> = Memory<T>;
-pub type Mem8 = Memory<u8>;
+pub type Mem8 = Memory<Byte>;
 pub type Mem16 = Memory<u16>;
 
 impl<T: Word + Zero> Memory<T> {
@@ -105,6 +102,10 @@ impl<T: Word + Zero> Memory<T> {
 impl<T: Word> Memory<T> {
     pub fn iter(&self) -> impl Iterator<Item = &T> {
         self.cells.iter()
+    }
+
+    pub fn size(&self) -> usize {
+        self.cells.len()
     }
 }
 
@@ -127,8 +128,21 @@ pub enum Instruction {
     #[default]
     Nop,
     Halt,
+    Show(RegAddress),
     Init(RegAddress, Imed),
     And(RegAddress, RegAddress),
+    Or(RegAddress, RegAddress),
+    Xor(RegAddress, RegAddress),
+    Add(RegAddress, RegAddress, RegAddress),
+    Sub(RegAddress, RegAddress, RegAddress),
+    Adi(RegAddress, Imed),
+    Not(RegAddress, RegAddress),
+    Shl(RegAddress, Imed),
+    Copy(RegAddress, RegAddress),
+    Go(Label),
+    Wz(RegAddress, Label),
+    Wneg(RegAddress, Label),
+    When(RegAddress, Label),
 }
 pub type Ins = Instruction;
 
@@ -147,13 +161,19 @@ pub enum RegAddress {
     Acc,
     Bacc,
     Carr,
-    Datt,
     Idx,
     Hi,
     Lo,
     Ret,
+    Tmp
 }
-use RegAddress::{Acc, Bacc, Carr, Datt, Hi, Idx, Lo, Ret};
+use RegAddress::{Acc, Bacc, Carr, Hi, Idx, Lo, Ret, Tmp};
+
+impl fmt::Display for RegAddress {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{self:#?}")
+    }
+}
 
 pub struct Harvard<I, D>
 where
@@ -168,33 +188,159 @@ where
 impl<D: Word> Harvard<Instruction, D> {
     pub fn execute(&mut self) {
         let regs = &mut self.cpu.bank;
+        let ins = &mut self.ins_mem;
 
-        for ins in self.ins_mem.iter() {
-            match ins {
-                Ins::Nop => continue,
-                Ins::Halt => break,
+        let mut cycles = 0;
+        let mut i = 0;
+
+        while i < ins.size() {
+            cycles += 1;
+            print!("{i}. ");
+            match ins[i] {
+                Ins::Nop => {
+                    i += 1;
+                    continue;
+                }
+                Ins::Halt => {
+                    println!("Halt!");
+                    break;
+                }
                 Ins::Init(rd, im) => {
-                    regs[*rd] = *im; 
-                },
+                    regs[rd] = im;
+                    println!("{rd} <- {im}");
+                }
+                Ins::Show(r) => {
+                    println!("{r} = {}", regs[r]);
+                }
                 Ins::And(rd, rs) => {
-                    regs[*rd] = regs[*rd] & regs[*rs];
-                },
-                x => println!("not impl: {x:#?}"),
+                    regs[rd] = regs[rd] & regs[rs];
+                    println!("{rd} <- {rd} & {rs}");
+                    println!("   {rd} = {}", regs[rd]);
+                }
+                Ins::Or(rd, rs) => {
+                    regs[rd] = regs[rd] | regs[rs];
+                    println!("{rd} <- {rd} | {rs}");
+                    println!("   {rd} = {}", regs[rd]);
+                }
+                Ins::Xor(rd, rs) => {
+                    regs[rd] = regs[rd] ^ regs[rs];
+                    println!("{rd} <- {rd} ^ {rs}");
+                    println!("   {rd} = {}", regs[rd]);
+                }
+                Ins::Add(rd, rs, rt) => {
+                    regs[rd] = regs[rs].wrapping_add(regs[rt]);
+                    println!("{rd} <- {rs} + {rt}");
+                    println!("   {rd} = {}", regs[rd]);
+                }
+                Ins::Sub(rd, rs, rt) => {
+                    regs[rd] = regs[rs].wrapping_sub(regs[rt]);
+                    println!("{rd} <- {rs} - {rt}");
+                    println!("   {rd} = {}", regs[rd]);
+                }
+                Ins::Adi(rd, im) => {
+                    regs[rd] = regs[rd].wrapping_add(im);
+                    println!("{rd} <- {rd} + {im}");
+                    println!("   {rd} = {}", regs[rd]);
+                }
+                Ins::Not(rd, rs) => {
+                    regs[rd] = !regs[rs];
+                    println!("{rd} <- !{rs}");
+                    println!("   {rd} = {}", regs[rd]);
+                }
+                Ins::Shl(rd, im) => {
+                    regs[rd] = regs[rd] << im;
+                    println!("{rd} <- {rd} << {im}");
+                    println!("   {rd} = {}", regs[rd]);
+                }
+                Ins::Copy(rd, rs) => {
+                    regs[rd] = regs[rs];
+                    println!("{rd} <- {rs}");
+                    println!("   {rd} = {}", regs[rd]);
+                }
+                Ins::Go(lbl) => {
+                    println!("PC <- {lbl}");
+                    println!("");
+                    i = lbl as usize;
+                    continue;
+                }
+                Ins::Wz(rd, lbl) => {
+                    println!("? {rd} == 0");
+                    if regs[rd] == 0 {
+                        println!("   True");
+                        println!("   PC <- {lbl}");
+                        println!("");
+                        i = lbl as usize;
+                        continue;
+                    } else {
+                        println!("   False");
+                        i += 1;
+                        println!("");
+                        continue;
+                    }
+                }
+                Ins::Wneg(rd, lbl) => {
+                    println!("? {rd} < 0");
+                    if regs[rd] > 127 {
+                        println!("   True");
+                        println!("   PC <- {lbl}");
+                        println!("");
+                        i = lbl as usize;
+                        continue;
+                    } else {
+                        println!("   False");
+                        i += 1;
+                        println!("");
+                        continue;
+                    }
+                }
+                Ins::When(rd, lbl) => {
+                    println!("? {rd}");
+                    if regs[rd] == 0xFF {
+                        println!("   True");
+                        println!("   PC <- {lbl}");
+                        println!("");
+                        i = lbl as usize;
+                        continue;
+                    } else {
+                        println!("   False");
+                        i += 1;
+                        println!("");
+                        continue;
+                    }
+                }
             }
+            println!("");
+            i += 1;
         }
-        println!("{:#?}", self.cpu.bank);    
-    }    
+        // println!("{:#?}", self.cpu.bank);
+        println!("\n\ncycles taken: {cycles}");
+    }
 }
+
+type Label = usize;
 
 fn main() {
     let root = Processor::new();
     let mut ins_mem: Mem<Ins> = Mem::new(256);
     let data_mem: Mem8 = Mem::new(256);
 
-    ins_mem[0] = Ins::Init(Acc, 13);
-    ins_mem[1] = Ins::Init(Bacc, 35);
-    ins_mem[2] = Ins::And(Acc, Bacc);
-    ins_mem[3] = Ins::Halt;
+    let _loop: Label = 2;
+    let _end: Label = 6;
+    ins_mem[0] = Ins::Init(Acc, 7);
+    ins_mem[1] = Ins::Init(Bacc, 5);
+    ins_mem[_loop] = Ins::Adi(Bacc, 255);
+    ins_mem[3] = Ins::Add(Tmp, Tmp, Acc);
+    ins_mem[4] = Ins::Wz(Bacc, _end);
+    ins_mem[5] = Ins::Go(_loop);
+    ins_mem[_end] = Ins::Copy(Acc, Tmp);
+    ins_mem[7] = Ins::Halt;
+
+
+    // ris! {
+    //     init Acc 3
+    //     init Bacc 5
+
+    // }
 
     let mut computer = Harvard {
         cpu: root,
@@ -204,3 +350,44 @@ fn main() {
 
     computer.execute();
 }
+
+    // Or: And + Not
+    // ins_mem[2] = Ins::Not(Acc, Acc);
+    // ins_mem[3] = Ins::Not(Tmp, Bacc);
+    // ins_mem[4] = Ins::And(Acc, Tmp);
+    // ins_mem[5] = Ins::Not(Acc, Acc);
+
+    // Mul: Add + Go
+    // let _loop: Label = 2;
+    // let _end: Label = 6;
+    // ins_mem[0] = Ins::Init(Acc, 7);
+    // ins_mem[1] = Ins::Init(Bacc, 5);
+    // ins_mem[_loop] = Ins::Adi(Bacc, 255);
+    // ins_mem[3] = Ins::Add(Tmp, Acc);
+    // ins_mem[4] = Ins::Wz(Bacc, _end);
+    // ins_mem[5] = Ins::Go(_loop);
+    // ins_mem[_end] = Ins::Copy(Acc, Tmp);
+    // ins_mem[7] = Ins::Halt;
+
+    // Xor: Or
+    // ins_mem[0] = Ins::Init(Acc, 3);
+    // ins_mem[1] = Ins::Init(Bacc, 5);
+    // ins_mem[2] = Ins::Copy(Tmp, Bacc);
+    // ins_mem[3] = Ins::And(Tmp, Acc);
+    // ins_mem[4] = Ins::Not(Tmp, Tmp);
+    // ins_mem[5] = Ins::Or(Acc, Bacc);
+    // ins_mem[6] = Ins::And(Acc, Tmp);
+    // ins_mem[7] = Ins::Halt;
+
+    // Xor: Not + And    
+    // ins_mem[0] = Ins::Init(Acc, 3);
+    // ins_mem[1] = Ins::Init(Bacc, 5);
+    // ins_mem[2] = Ins::Copy(Tmp, Bacc);
+    // ins_mem[3] = Ins::And(Tmp, Acc);
+    // ins_mem[4] = Ins::Not(Tmp, Tmp);
+    // ins_mem[5] = Ins::Not(Acc, Acc);
+    // ins_mem[6] = Ins::Not(Carr, Bacc);
+    // ins_mem[7] = Ins::And(Acc, Carr);
+    // ins_mem[8] = Ins::Not(Acc, Acc);
+    // ins_mem[9] = Ins::And(Acc, Tmp);
+    // ins_mem[10] = Ins::Halt;
